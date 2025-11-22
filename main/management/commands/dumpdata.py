64bf2932ab36e0d08fe1b73937b50b3452340a1a
salismazaya <salismazaya@gmail.com>
2025-11-22 16:25:53 +0700

    ✨ (management/commands): add dumpdata and loaddata commands for data serialization

---
+++ main/management/commands/dumpdata.py
@@ -0,0 +1,31 @@
+from django.core.management import BaseCommand
+from main.models import User, Kelas, Absensi, KunciAbsensi, Siswa
+from django.core import serializers
+import pickle
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('output-path', type = str)
+
+    def execute(self, *args, **options):
+        users = User.objects.all()
+        kelass = Kelas.objects.all()
+        siswas = Siswa.objects.all()
+        kuncis = KunciAbsensi.objects.all()
+        absensies = Absensi.objects.all()
+
+        result = {}
+        result['users'] = serializers.serialize('python', users)
+        result['kelass'] = serializers.serialize('python', kelass)
+        result['siswas'] = serializers.serialize('python', siswas)
+        result['kuncis'] = serializers.serialize('python', kuncis)
+        result['absensies'] = serializers.serialize('python', absensies)
+
+        for x in result['absensies'] + result['kuncis']:
+            # id tabel diatas tidak di-import
+            del x['pk']
+
+        output = pickle.dumps(result)
+
+        open(options['output-path'], 'wb').write(output)
+
