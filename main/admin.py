bf7db2ab9140e6d88b7784a236c15a6f048302c0
salismazaya <salismazaya@gmail.com>
2025-11-25 22:53:49 +0700

    kasep

--- main/admin.py
+++ main/admin.py
@@ -9,7 +9,7 @@ from django.contrib import messages
 
 # from django.contrib.auth.forms import UserChangeForm
 from main.forms import UserCreationForm, createKelasForm, createUserChangeForm
-from main.models import Absensi, Kelas, KunciAbsensi, Siswa, User, Domain
+from main.models import Absensi, Kelas, KunciAbsensi, Siswa, User
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -26,7 +26,7 @@ class AdminSite(admin.AdminSite):
         if not old_kelas_id or not new_kelas_name:
             return HttpResponse('NOT_OK')
         
-        old_kelas = Kelas.original_objects.filter(pk = old_kelas_id).first()
+        old_kelas = Kelas.objects.filter_domain(request).filter(pk = old_kelas_id).first()
         if old_kelas is None:
             return HttpResponse('NOT_OK')
         
@@ -36,7 +36,7 @@ class AdminSite(admin.AdminSite):
         old_kelas.active = False
         old_kelas.save()
 
-        Siswa.original_objects.filter(
+        Siswa.objects.filter_domain(request).filter(
             kelas__pk = old_kelas_id
         ).update(
             kelas_id = new_kelas.pk
@@ -61,13 +61,8 @@ class AdminSite(admin.AdminSite):
 
 class FilterDomainMixin:
     def get_queryset(self, request):
-        return super().get_queryset(request)
-    
-    def save_model(self, request: HttpRequest, obj, form, change):
-        domain = request.get_host()
-        
-
-        return super().save_model(request, obj, form, change)
+        rv = super().get_queryset(request)
+        return rv
 
 
 class CustomAuthUserAdmin(FilterDomainMixin, AuthUserAdmin):
