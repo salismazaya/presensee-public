d88299c6a8cf37fb2559fb80b5ac3cb4a9786e80
salismazaya <salismazaya@gmail.com>
2025-12-12 08:36:19 +0700

    ✨ Add Data model for school information and user photo field
    The changes introduce a new `Data` model to store school-related information such as name, logo, description, and letterhead. Additionally, a `photo` field is added to the `User` model to support user profile pictures. These changes enhance the application's functionality by allowing customization of school data and user profiles.

--- main/models/__init__.py
+++ main/models/__init__.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-from .models import (Absensi, AbsensiSession, Kelas,  # noqa: F401
+from .models import (Absensi, AbsensiSession, Data, Kelas,  # noqa: F401
                      KunciAbsensi, Siswa, User)
-
-=======
-from .models import (Absensi, Data, Kelas, KunciAbsensi, Siswa,  # noqa: F401
-                     User)
-
->>>>>>> feat/data
