67b6b739c8567c89a82b8f970ff0c3e48fb17ac3
salismazaya <salismazaya@gmail.com>
2025-12-22 13:05:00 +0700

    🔧 (upload.py): reformat import statement for better readability
    🐛 (signals.py): fix photo deletion to avoid unnecessary database save
    💡 (signals.py): add comment to clarify the reason for save=False

--- main/signals.py
+++ main/signals.py
@@ -1,7 +1,9 @@
-from django.db.models.signals import pre_save, pre_delete
-from django.dispatch import receiver
-from main.models import Siswa, User
 from typing import Union
+
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
+
+from main.models import Siswa, User
 
 
 @receiver(pre_delete, sender=User)
@@ -11,8 +13,8 @@ def delete_photo_handler(sender, **kwargs):
 
     instance = kwargs["instance"]
     if instance.photo:
-        # save=True invoke .save()
-        instance.photo.delete(save=True)
+        # tidak emit ke db, karena akan dihapus
+        instance.photo.delete(save=False)
 
 
 @receiver(pre_save, sender=User)
