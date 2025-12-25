1f2e6e46c54735037fa05ffd80d3ad09fa3655ca
salismazaya <salismazaya@gmail.com>
2025-12-25 18:01:39 +0700

    add pkg

---
+++ frontend/src/pkg/presensee_wasm_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const get_author_name: () => [number, number];
+export const get_author_site: () => [number, number];
+export const get_github_url: () => [number, number];
+export const get_welcome_message: () => [number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
